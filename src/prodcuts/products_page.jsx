10154dbf32b42a91373621d18decaf4469d5e360
Muhammad Ali <113182061+MAliSheikh@users.noreply.github.com>
2024-10-28 19:05:37 +0500

    logout functionality added

--- src/prodcuts/products_page.jsx
+++ src/prodcuts/products_page.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, Button } from '@mui/material';
+import authService from '../components/LoginSignup/components/token'
+import { replace, useNavigate } from 'react-router-dom';
+
 
 const Products = () => {
+    const navigate = useNavigate(); 
+    
+    const handleLogout = () => {
+        authService.logout();
+        navigate('/login', { replace: true })
+    };
+
     return (
         <Box>
             <Typography variant="h4">Welcome to the Dashboard!</Typography>
             <Typography variant="body1">You have successfully logged in.</Typography>
+
+
+            <Button
+                onClick={handleLogout}
+            >
+                Logout
+            </Button>
         </Box>
     );
 };
