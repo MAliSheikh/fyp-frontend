63caef1271f92fb52cccc068347c557ba98e42cc
Muhammad Ali <113182061+MAliSheikh@users.noreply.github.com>
2024-12-06 16:03:43 +0500

    add other 3d model of shirt

--- src/3d_viewer/viewer.jsx
+++ src/3d_viewer/viewer.jsx
@@ -6,22 +6,24 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Environment } from "@react-three/drei";
 import Earth from "./Earth";
 import Tshirt from "./Tshirt";
+import Shirt from "./Shirt";
 
 function Viewer() {
-  const [currentModel, setCurrentModel] = useState('earth');
+  const [currentModel, setCurrentModel] = useState('shirt');
 
   return (
     <Box sx={{ width: '100%', height: '500px' }}>
       <Typography variant="body1" color="initial">3D Object Viewer</Typography>
-      <Button onClick={() => setCurrentModel('earth')}>Show Earth</Button>
-      <Button onClick={() => setCurrentModel('tshirt')}>Show T-shirt</Button>
+      {/* <Button onClick={() => setCurrentModel('earth')}>Show Earth</Button> */}
+      <Button onClick={() => setCurrentModel('shirt')}>Show shirt</Button>
+      <Button onClick={() => setCurrentModel('tshirt')}>Show t-shirt</Button>
 
       {/* <Canvas camera={{ position: [0, 0, 2], fov: 45 }}> */}
       <Canvas >
         <ambientLight intensity={2} />
         <OrbitControls enableZoom={true} enablePan={true} />
         <Suspense fallback={null}>
-          {currentModel === 'earth' ? <Earth /> : <Tshirt />}
+          {currentModel === 'shirt' ? <Shirt /> : <Tshirt />}
           <Environment preset="sunset" />
         </Suspense>
       </Canvas>
